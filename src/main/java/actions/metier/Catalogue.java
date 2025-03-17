acef3b972e5f414057b798a86e7971b2f440894b
Etienne <etienne.coyac@gmail.com>
2025-03-17 16:25:43 +0100

    [doc] added javadoc

--- src/main/java/actions/metier/Catalogue.java
+++ src/main/java/actions/metier/Catalogue.java
@@ -2,32 +2,62 @@ package actions.metier;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+/**
+ * Catalogue général d'actions disponibles dans l'application
+ */
 public class Catalogue {
+    /**
+     * Liste des actions
+     */
     private List<Action> actions;
 
     public Catalogue() {
         this.actions = new ArrayList<>();
     }
 
+    /**
+     * ajouter une action au catalogue
+     * 
+     * @param a action
+     */
     public void addAction(Action a) {
         this.actions.add(a);
     }
 
+    /**
+     * Get la liste d'actions (non modifiables pour préserver l'encapsulation)
+     * 
+     * @return la liste d'actions
+     */
     public Collection<Action> getActions() {
-        return this.actions;
+        return Collections.unmodifiableCollection(this.actions);
     }
 
-    public void deleteActionByName(String name) {
+    /**
+     * Supprimer une action par nom
+     * 
+     * @param name nom de l'action
+     * @throws NoSuchElementException
+     */
+    public void deleteActionByName(String name) throws NoSuchElementException {
         boolean deleted = this.actions.removeIf(a -> a.getNom().equals(name));
         if (!deleted) {
             throw new NoSuchElementException("L'action " + name + " n'existe pas.");
         }
     }
 
+    /**
+     * Get une action par nom
+     * 
+     * @param name nom de l'action
+     * @return l'action trouvée
+     * @throws NoSuchElementException
+     */
     public Action getActionByName(String name) throws NoSuchElementException {
         Optional<Action> action = this.actions.stream().filter(a -> name.equals(a.getNom())).findFirst();
         if (action.isPresent()) {
