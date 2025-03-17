cfdf120a89b7673135672b6cea565ce1f21d3645
Gu HJ <guhongjin2099@gmail.com>
2025-03-17 14:57:54 +0100

    [feat] first version of action composee avec fonc: getvaleur, addActionsimple, RemoveActionsimple

--- src/main/java/actions/metier/ActionComposee.java
+++ src/main/java/actions/metier/ActionComposee.java
@@ -1,6 +1,73 @@
 
 package actions.metier;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+
 public class ActionComposee extends Action {
+    private HashMap<ActionSimple, BigDecimal> ListActionSimple;
+    private String libelle;
+    private String nom;
+    private String description;
+
+    public ActionComposee(String libelle, String nom, String description) {
+        super(libelle,nom,description);
+        ListActionSimple = new HashMap<ActionSimple, BigDecimal>();
+    }
+
+    public ActionComposee(){
+        super();
+        ListActionSimple = new HashMap<ActionSimple, BigDecimal>();
+    }
+    
+
+    public void getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public void getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void addActionsimple(ActionSimple actionSimple, BigDecimal pourcentage) {
+        ListActionSimple.put(actionSimple, pourcentage);
+    }
+
+    public void removeActionsimple(ActionSimple actionSimple) {
+        ListActionSimple.remove(actionSimple);
+    }
+
+    public void getValeur(Date date) {
+        //ici on va calculer la valeur de l'action composée
+        //faut implémenter la méthode getValeurByDate dans ActionSimple/cours
+        BigDecimal valeur = new BigDecimal(0);
+        for (ActionSimple actionSimple : ListActionSimple.keySet()) {
+            valeur = valeur.add(actionSimple.getValeur(date).multiply(ListActionSimple.get(actionSimple)));
+        }
+        return valeur;
+    }
+
+    
+
+
+
+
+
 
 }
