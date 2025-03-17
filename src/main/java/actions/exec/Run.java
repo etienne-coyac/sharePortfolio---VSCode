63534deb7caee39a2edc67be00b4ef8021263340
Etienne <etienne.coyac@gmail.com>
2025-03-17 14:17:22 +0100

    [setup] cleaned code

---
+++ src/main/java/actions/exec/Run.java
@@ -0,0 +1,10 @@
+
+package actions.exec;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/actions/metier/Catalogue.java
@@ -0,0 +1,5 @@
+package actions.metier;
+
+public class Catalogue {
+
+}
